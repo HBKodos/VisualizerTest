68979be606975629d9d00e34572cb5663283279d
HBKogos <bobby.kogos@icloud.com>
2024-04-07 15:06:06 -0400

    Initial Commit

---
+++ VisualizerTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisualizerTest
+//
+//  Created by Haralambos Kogos on 2024.04.07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisualizerTest/VisualizerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualizerTestApp.swift
+//  VisualizerTest
+//
+//  Created by Haralambos Kogos on 2024.04.07.
+//
+
+import SwiftUI
+
+@main
+struct VisualizerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
