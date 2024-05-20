41d5f1126240e0a89ff55edc6be1256ebc8966df
HBKogos <bobby.kogos@icloud.com>
2024-05-20 18:01:10 -0400

    Prototype model migrated. 💪

--- PrototypeView.swift
+++ PrototypeView.swift
@@ -7,89 +7,51 @@
 
 import SwiftUI
 
-enum SegmentShape: View {
-    case rectangle, circle
-    //    case roundedRectangle(cornerRadius: CGFloat)
-    //    var cornerRadius: CGFloat
-    
-    var body: some View {
-        switch self {
-        case .rectangle:
-            Rectangle()
-        case .circle:
-            Circle()
-            //        case .roundedRectangle:
-            //            RoundedRectangle(cornerRadius: 10)
-        }
-    }
-    
-}
 
-struct PrototypeSegment: Identifiable, View {
-    let id = UUID()
-    var amp: Double = { Double.random(in: 0.0..<1.0) }()
-    var freq: Double = { Double.random(in: 0.0..<20000.0) }()
-    var hue: Double = 0.98
-    var color: Color { Color(hue: self.hue,
-                             saturation: 1.0,
-                             brightness: 1.0,
-                             opacity: 1.0)
-    }
-    var shape: SegmentShape = .rectangle
-    var body: some View {
-        shape
-            .foregroundColor(color)
-            .cornerRadius(10)
-        //            .frame(height: .infinity)
-    }
-    
-}
 
-struct PrototypeView: View {
-    @State private var segments: [PrototypeSegment] = []
-    
-    var body: some View {
-        VStack {
-            GeometryReader { geo in
-                HStack(alignment: .bottom ,spacing: 2) {
-                    ForEach(segments) { segment in
-                        PrototypeSegment()
-                            .frame(height: geo.size.height * segment.amp)
-                            .animation(.default, value: segment.amp)
-                            .onTapGesture {
+//struct PrototypeView: View {
+//    @State private var segments: [PrototypeSegment] = []
+//    
+//    var body: some View {
+//        VStack {
+//            GeometryReader { geo in
+//                HStack(alignment: .bottom ,spacing: 2) {
+//                    ForEach(segments) { segment in
+//                        PrototypeSegment()
+//                            .frame(height: geo.size.height * segment.amp)
+//                            .animation(.default, value: segment.amp)
+//                            .onTapGesture {
 //                                print("butt")
-                                print("Hue: \(segment.hue)\nAmp: \(segment.amp)\nFreq: \(segment.freq)")
-//                                print(geo.frame(in: <#T##CoordinateSpaceProtocol#>))
-                            }
-                    }
-                }
-                .frame(height: .infinity)
-                .padding()
-                .onAppear {
-                    generatePTSegments()
-                }
-            }
-            Button("Wawiwawa", action: randomizePTAmps)
-        }
-        .background(.yellow)
-        .onTapGesture {
-            segments.enumerated().forEach { index, segment in
-                print("Segment#\(index) Amp: \(segment.amp)")
-            }
-        }
-    }
-    func generatePTSegments() {
-        for _ in 0..<10 {
-            segments.append(PrototypeSegment())
-        }
-    }
-    func randomizePTAmps() {
-        for segment in self.segments.indices {
-            self.segments[segment].amp = Double.random(in: 0.0..<1.0)
-        }
-    }
-}
-
-#Preview {
-    PrototypeView()
-}
+//                                print("Hue: \(segment.hue)\nAmp: \(segment.amp)\nFreq: \(segment.freq)")
+//                                print("_ Center: \(geo.frame(in: .global).midX)")
+//                            }
+//                    }
+//                }
+//                .frame(height: .infinity)
+//                .padding()
+//                .onAppear {
+//                    generatePTSegments()
+//                }
+//            }
+//            Button("Wawiwawa", action: randomizePTAmps)
+//        }
+//        
+//        // #warning unnecessary/debug BG
+//        .background(.white)
+//        .onTapGesture {
+//            segments.enumerated().forEach { index, segment in
+//                print("Segment#\(index) Amp: \(segment.amp)")
+//            }
+//        }
+//    }
+//    func generatePTSegments() {
+//        for _ in 0..<10 {
+//            segments.append(PrototypeSegment())
+//        }
+//    }
+    
+//}
+//
+//#Preview {
+//    PrototypeView()
+//}
--- VisualizerTest/ContentView.swift
+++ VisualizerTest/ContentView.swift
@@ -6,51 +6,66 @@
 //
 import SwiftUI
 
-struct Segment: Identifiable {
-    var id = UUID()
-    var amplitude: Double = 0.0
-    var frequency: Double = 0.0
-    var color: Color = Color.red
+enum SegmentShape: View {
+    case rectangle, circle
+    //    case roundedRectangle(cornerRadius: CGFloat)
+    //    var cornerRadius: CGFloat
+    
+    var body: some View {
+        switch self {
+        case .rectangle:
+            Rectangle()
+        case .circle:
+            Circle()
+            //        case .roundedRectangle:
+            //            RoundedRectangle(cornerRadius: 10)
+        }
+    }
+    
+}
+
+struct Segment: Identifiable, View {
+    let id = UUID()
+    var amp: Double = { Double.random(in: 0.0..<1.0) }()
+    var freq: Double = { Double.random(in: 0.0..<20000.0) }()
+    var hue: Double = 0.98
+    var color: Color { Color(hue: self.hue,
+                             saturation: 1.0,
+                             brightness: 1.0,
+                             opacity: 1.0)
+    }
+    var shape: SegmentShape = .rectangle
+    var body: some View {
+        shape
+            .foregroundColor(color)
+            .cornerRadius(10)
+    }
+    
 }
 
 @Observable
 final class VisualizerManager {
     var noOfSegments: Int = 10
     var segments: [Segment] = []
-    var currentHue: Double = 0.0
     let frequencyRangeArray: [Int] = [Int](20..<20000)
     let frequencyRange: Range = 20..<20000
     let colors: [Color] = [.purple, .red, .black]
     
     init() {
-        generateSegments(noOfSegmnents: noOfSegments, hue: &self.currentHue)
+        generateSegments(noOfSegmnents: noOfSegments, color: colors.randomElement()!)
     }
     
-    func changeHue(currentHue: Double, noOfSegments: Int) {
-        let hueDelta: Double = 1000.0 / Double(noOfSegments)
-        self.currentHue = currentHue + hueDelta
-        if self.currentHue > 99.9 {
-            self.currentHue -= 100
-        }
-    }
-    
-    func generateSegments(noOfSegmnents: Int, hue: inout Double) {
-        for _ in 0..<noOfSegmnents {
+    func generateSegments(noOfSegmnents: Int, color: Color) {
+        for _ in 0..<10 {
             segments.append(Segment())
-            changeHue(currentHue: hue, noOfSegments: noOfSegmnents)
         }
     }
     
     func randomizeAmps() {
         for segment in self.segments.indices {
-            self.segments[segment].amplitude = Double(frequencyRange.randomElement()!)
+            self.segments[segment].amp = Double.random(in: 0.0..<1.0)
         }
     }
-//    func randomizeColors() {
-//        segments.forEach { segment in
-//            ch
-//        }
-//    }
 }
 
 struct ContentView: View {
@@ -58,46 +73,26 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            HStack(spacing: 1) {
-                ForEach(visualizer.segments) { segment in
-                    segmentView
-                        .frame(height: 1 * segment.amplitude / 100)
-                        .animation(.default, value: segment.amplitude)
-                        .onTapGesture {
-//                            segment.amplitude = visualizer.frequencyRange.randomElement()
-                        }
-                }
-            }
-            .frame(maxHeight: .infinity)
-            .cornerRadius(30)
-            .padding(.top)
-            HStack {
-                Text("Tunesplash")
-                Button("Brrt", systemImage: "dice", action: visualizer.randomizeAmps)
-                    .buttonStyle(.bordered)
-//                Button("Oooh", systemImage: "paintpalette", action: )
-//                    .buttonStyle(.bordered)
-                Picker("#", selection: $visualizer.noOfSegments) {
-                    ForEach(0..<10, id: \.self) {
-                        Text("\($0)")
+            GeometryReader { geo in
+                HStack(alignment: .bottom ,spacing: 2) {
+                    ForEach(visualizer.segments) { segment in
+                        Segment()
+                            .frame(height: geo.size.height * segment.amp)
+                            .animation(.default, value: segment.amp)
+                            .onTapGesture {
+                                print("butt")
+                                print("Hue: \(segment.hue)\nAmp: \(segment.amp)\nFreq: \(segment.freq)")
+                                print("_ Center: \(geo.frame(in: .global).midX)")
+                            }
                     }
                 }
-
+                .frame(height: .infinity)
+                .padding()
             }
-            .onChange(of: visualizer.noOfSegments) {
-//                gener
-            }
+            Button("Wawiwawa", action: visualizer.randomizeAmps)
         }
-        .onAppear(perform: visualizer.randomizeAmps)
+//        .onAppear(perform: visualizer.randomizeAmps)
         .background(.black)
-    }
-    var segmentView: some View {
-        Rectangle()
-            .fill(.red.gradient)
-//            .fill(Color(hue: visualizer.currentHue)
-            .brightness(-0.4)
-            .cornerRadius(16)
-//            .padding(.top)
     }
 }
 
